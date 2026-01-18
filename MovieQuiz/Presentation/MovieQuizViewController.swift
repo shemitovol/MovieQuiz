27344390329737e92942e22603a01abf61e28b8a
shemitov.ol <shemitov.ol@yandex.com>
2026-01-18 16:31:19 +0500

    Реализована работа с сетью: запрос фильмов из API IMDb топ 250

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -13,17 +13,22 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
     private var correctAnswers = 0
     private var answerButtons: [UIButton] { [yesButton, noButton] }
     private var questionsAmount: Int = 10
-    private var questionFactory: QuestionFactoryProtocol = QuestionFactory()
+    private var questionFactory: QuestionFactoryProtocol?
     private var currentQuestion: QuizQuestion?
     private var alertPresenter = AlertPresenter()
     private var statisticService: StatisticServiceProtocol = StatisticService()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let questionFactory = QuestionFactory()
+        showLoadingIndicator()
+        questionFactory = QuestionFactory(moviesLoader: MoviesLoader(), delegate: self)
+        statisticService = StatisticService()
+        questionFactory?.loadData()
+        
+        /*let questionFactory = QuestionFactory()
         questionFactory.setup(delegate: self)
         self.questionFactory = questionFactory
-        questionFactory.requestNextQuestion()
+        questionFactory.requestNextQuestion()*/
     }
     
     //MARK: - QuestionFactoryDelegate
@@ -38,10 +43,19 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
         }
     }
     
+    func didLoadDataFromServer() {
+        hideLoadingIndicator()
+        questionFactory?.requestNextQuestion()
+    }
+    
+    func didFailToLoadData(with error: Error){
+        showNetworkError(message: error.localizedDescription)
+    }
+    
     //MARK: - Private functions
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
         QuizStepViewModel(
-            image: UIImage(named: model.imageName) ?? UIImage(),
+            image: UIImage(data: model.image) ?? UIImage(),
             question: model.text,
             questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)"
         )
@@ -82,7 +96,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
             show(quiz: viewModel)
         } else {
             currentQuestionIndex += 1
-            questionFactory.requestNextQuestion()
+            questionFactory?.requestNextQuestion()
         }
         imageView.layer.borderWidth = 0
         setButtonsEnabled(true)
@@ -100,7 +114,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
             guard let self = self else {return}
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
-            self.questionFactory.requestNextQuestion()
+            self.questionFactory?.requestNextQuestion()
         }
         alertPresenter.show(in: self, model: model)
     }
@@ -120,7 +134,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
         activityIndicator.isHidden = true
     }
     
-    private func howNetworkError (message: String) {
+    private func showNetworkError (message: String) {
         hideLoadingIndicator()
         let model = AlertModel(
             title: "Ошибка",
@@ -129,7 +143,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
                 guard let self = self else {return}
                 self.currentQuestionIndex = 0
                 self.correctAnswers = 0
-                self.questionFactory.requestNextQuestion()
+                self.questionFactory?.requestNextQuestion()
             }
         alertPresenter.show(in: self, model: model)
     }
--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 final class QuestionFactory: QuestionFactoryProtocol {
-    private let questions: [QuizQuestion] = [
+    /*private let questions: [QuizQuestion] = [
         QuizQuestion(imageName: "The Godfather",
                      text: "Рейтинг этого фильма больше, чем 6?",
                      correctAnswer: true),
@@ -33,19 +33,68 @@ final class QuestionFactory: QuestionFactoryProtocol {
                      text: "Рейтинг этого фильма больше, чем 6?",
                      correctAnswer: false)
     ]
+    */
+    private let moviesLoader: MoviesLoading
+    private var movies: [MostPopularMovie] = []
+    private weak var delegate: QuestionFactoryDelegate?
     
-    weak var delegate: QuestionFactoryDelegate?
+    init(moviesLoader: MoviesLoading, delegate: QuestionFactoryDelegate?){
+        self.moviesLoader = moviesLoader
+        self.delegate = delegate
+    }
     
     func setup(delegate: QuestionFactoryDelegate?){
         self.delegate = delegate
     }
     
-    func requestNextQuestion () {
+    func loadData() {
+        moviesLoader.loadMovies { [weak self] result in
+            DispatchQueue.main.async {
+                guard let self = self else {return}
+                switch result {
+                case .success(let mostPopularMovies):
+                    self.movies = mostPopularMovies.items
+                    self.delegate?.didLoadDataFromServer()
+                case .failure(let error):
+                    self.delegate?.didFailToLoadData(with: error)
+                }
+            }
+        }
+    }
+    
+/*    func requestNextQuestion () {
         guard let index = (0..<questions.count).randomElement() else {
             delegate?.didReceiveNextQuestion (question: nil)
             return
         }
         let question = questions[safe: index]
         delegate?.didReceiveNextQuestion (question: question)
+    }
+ */
+    func requestNextQuestion() {
+        DispatchQueue.global().async { [weak self] in
+            guard let self = self else {return}
+            let index = (0..<self.movies.count).randomElement() ?? 0
+            
+            guard let movie = self.movies[safe: index] else {return}
+            var imageData = Data()
+            do {
+                imageData = try Data(contentsOf: movie.resizedImageURL)
+            } catch {
+                print("Failure to load image")
+            }
+            
+            let rating = Float(movie.rating) ?? 0
+            let text = "Рейтинг этого фильма больше, чем 7?"
+            let correctAnswer = rating > 7
+            let question = QuizQuestion(image: imageData,
+                                        text: text,
+                                        correctAnswer: correctAnswer)
+            
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else {return}
+                self.delegate?.didReceiveNextQuestion(question: question)
+            }
+        }
     }
 }
